2ea87d191b0d55f96a5a6e8105737a86128f029b
jenser2144 <joeyenser@Joeys-MacBook-Pro.local>
2021-04-23 22:52:48 -0600

    updated get_toggl_projects method to pull data into list of dictionaries

--- toggl_extract.py
+++ toggl_extract.py
@@ -42,21 +42,11 @@ class TogglApi():
         Args:
 
         Returns:
-            dataframe object: Pandas dataframe containing toggl projects data
+            projects_data (list): List of dictionaries containing projects data
         
         """
-        # TODO: possibly re-do this to not use pandas?
         projects_data = self.client.get_projects().json()
-        projects_df = pd.DataFrame(projects_data)
-
-        # data cleanup
-        projects_df["at"] = pd.to_datetime(projects_df["at"])
-        projects_df["created_at"] = pd.to_datetime(projects_df["created_at"])
-        convert_to_string = ["billable", "is_private", "active", "template", "auto_estimates"]
-        for col in convert_to_string:
-            projects_df[col] = projects_df[col].astype(str)
-        
-        return projects_df
+        return projects_data
 
     def get_toggl_log_data(self, project_id, date_range_list):
         """Pull toggl data from api
